17050a66113d5f5ac5ed99c23c49fb193929ee6d
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-10-06 00:26:02 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records) {
+  const result = records.find(record => record.result === "W");
+  if (result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
+}
